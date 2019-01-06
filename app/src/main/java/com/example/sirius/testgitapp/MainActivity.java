3aeb009b3f6703c15426c973cc74cb430c07586b
Antihype Zamay <art16ws@gmail.com>
2019-01-06 16:25:02 +0300

    aaa

--- app/src/main/java/com/example/sirius/testgitapp/MainActivity.java
+++ app/src/main/java/com/example/sirius/testgitapp/MainActivity.java
@@ -2,12 +2,22 @@ package com.example.sirius.testgitapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
 }
